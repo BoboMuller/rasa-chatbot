7c823686b14e508b0cc02b4cd935d66f7063befb
RobertGess <robert.gess@t-online.de>
2021-11-25 23:37:15 +0100

    Flask, API calls to DB

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,26 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+#Nicht getestet
+class get_train_id(Location):
+    plan = requests.get("https://api.deutschebahn.com/freeplan/v1/location/" + Location)
+    plan = plan.json()
+    return plan[0]["id"]
+
+class ActionHelloWorld(Action):
+
+    def name(self) -> Text:
+        return "action_details"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(text="Hello World!")
+
+        return []
